2010681932202af22661b9dd396176dc41d082f2
Rodri ananto <rodri.ant48s@gmail.com>
2017-03-17 14:29:40 +0700

    Update jsonrdr2.php

--- jsonrdr2.php
+++ jsonrdr2.php
@@ -5,20 +5,17 @@ $array = Array (
         "nim" => "15.01.53.0117",
         "name" => "Rodri ananto",
         "alamat" => "Kaliwungu",
-		"no hp" => "083xxx",
+		"no hp" => "083866013066",
 		"matkul" => ["web service", "Teknologi Web", "Grafika Komputer"]
 ));
 
-// encode array to json
+// ubah encode ke array
 $json = json_encode(array('data' => $array));
 
-// write json to file
+// keterangan
 if (file_put_contents("data.json", $json))
     echo "File JSON sukses dibuat...";
 else 
     echo "Oops! Terjadi error saat membuat file JSON...";
 
-// data.json
-
-// {"data":[{"id":"USR1","name":"Steve Jobs","alamat":"Apple"},{"id":"USR2","name":"Bill Gates","alamat":"Microsoft"},{"id":"USR3","name":"Mark Zuckerberg","alamat":"Facebook"}]}
 ?>
